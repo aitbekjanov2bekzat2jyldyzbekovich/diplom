3af2f5637d1ca8b6b3889a99deafcf1175f3a19d
bekzat <bekzatajtbekzanov@gmail.com>
2026-02-12 16:56:38 +0600

    net message

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,14 @@ export default {
     this.appStore.resizeWindow()
     this.appStore.initAuthListener()
     this.appStore.fetchCourses()
+
+    window.addEventListener('offline', () => {
+      this.appStore.message('Интернет соединение потеряно', 'red')
+    })
+
+    window.addEventListener('online', () => {
+      this.appStore.message('Интернет соединение восстановлено', 'green')
+    })
   },
 }
 </script>
